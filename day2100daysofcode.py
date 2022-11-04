7aba80f27e23e03e1927d511af9f18c38cb5fb27
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2022-11-05 00:28:41 +0530

    Add files via upload
    inputoutput

---
+++ day2100daysofcode.py
@@ -0,0 +1,6 @@
+https://replit.com/@ChaitanyaKatore/day2100-days#main.py
+name = input("what is your name: ")
+myAge = input("what is your age: ")
+print("Hellow " + name)
+
+print("welcome to this site we are glad you have started this things")
