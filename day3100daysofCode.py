e4e478cc12550257126dc97b0e84a2bd1e274fbc
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2022-11-08 01:27:35 +0530

    Add files via upload
    day3 of 100days python learn concatenation in this tutorial

---
+++ day3100daysofCode.py
@@ -0,0 +1,9 @@
+#day3 100daysofPython
+#concatenation
+
+myName = input("what is your name? ")
+myLunch = input("what are you having for lunch? ")
+#just need to add varible name and string that we want to add in double cotetion
+print(myName, "is good person")
+
+
