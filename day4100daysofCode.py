9f1c44282cb3badc81947e3707c4083265371466
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2022-11-08 01:47:43 +0530

    Add files via upload
    simple adventure simulatore paragraph using python

---
+++ day4100daysofCode.py
@@ -0,0 +1,9 @@
+#Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!
+
+name = input("what is your name? ")
+enemy = input("what is your worst enemy's name? ")
+superpower = input("what is your superpower? ")
+favoriteFood = input("what is your favorite food? ")
+address = input("what is your address ? ")
+
+print(name ,"Your ability to", superpower, "will make sure you never have to look at",enemy ,"again. Go eat ",favoriteFood," as you walk down the streets of",address," and use" ,superpower," for good and not evil!")
